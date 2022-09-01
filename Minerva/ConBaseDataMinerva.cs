bfc76c500a8f7c125bee1c05411528540eea45f6
Deoniks <55949787+Deoniks@users.noreply.github.com>
2022-09-01 22:33:20 +0600

    Add files via upload

---
+++ Minerva/ConBaseDataMinerva.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace Minerva
+{
+    class ConBaseDataMinerva
+    {
+        private SQLiteConnection MinervaConn;
+
+        public void MinervaOpenConnection()
+        {
+            MinervaConn = new SQLiteConnection("Data Source = Minerva.db");
+
+            if(MinervaConn.State == System.Data.ConnectionState.Closed)
+            {
+                MinervaConn.Open();
+            }
+        }
+
+        public void MinervaCloseConection()
+        {
+            if (MinervaConn.State == System.Data.ConnectionState.Open)
+            {
+                MinervaConn.Close();
+            }
+        }
+
+        public SQLiteConnection MinervaGetConnection()
+        {
+            return MinervaConn;
+        }
+
+    }
+}
